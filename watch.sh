1b6cfc2da0ad832f86b3a56d521060df4360af7e
Andrey Zheleznov <zheland.net@gmail.com>
2022-08-29 08:31:43 +0300

    Add file size and better spinner to watch

--- watch.sh
+++ watch.sh
@@ -6,25 +6,28 @@
 
 set -e
 
-clocks=""
-clocks="$clocks\U0001f550\U0001f551\U0001f552\U0001f553\U0001f554\U0001f555"
-clocks="$clocks\U0001f556\U0001f557\U0001f558\U0001f559\U0001f55a\U0001f55b"
-
 mkdir -p target/touch
 mkdir -p target/problems
 
 while true; do
-    for i in $( seq 1 12 ); do
+    for ch in $( echo "⣼⣹⢻⠿⡟⣏⣧⣶" | sed -e 's/\(.\)/\1\n/g' ); do
         for bin_path in src/*.rs; do
             bin_name=$(basename -- "$bin_path" .rs)
             touch_path="target/touch/$bin_name"
             if [[ $touch_path -ot $bin_path ]]; then
                 touch $touch_path
                 cargo test --bin "$bin_name" || true
+                size=$(stat --printf="%s" "$bin_path")
+                if [ "$size" -gt "65536" ]; then
+                    printf '%s size: \e[0;31m%+5s/65536\e[0m bytes\n' "$bin_path" "$size"
+                elif [ "$size" -gt "60000" ]; then
+                    printf '%s size: \e[0;33m%+5s/65536\e[0m bytes\n' "$bin_path" "$size"
+                else
+                    printf '%s size: \e[0;32m%+5s/65536\e[0m bytes\n' "$bin_path" "$size"
+                fi
             fi
         done
-        printf " $( printf $clocks | head -c $((i * 4)) | tail -c 4 )\r"
+        printf " %s\r" "$ch"
         sleep 0.1;
-        printf "  \r"
     done
 done
